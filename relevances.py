2121378aa7f5755f3f2eabda28ef9a97a29b6344
Cait <caitriggs@gmail.com>
2017-09-11 13:50:04 -0700

    Add global relevance_bias var to relevances.py functions

--- relevances.py
+++ relevances.py
@@ -2,21 +2,25 @@
 # return a list of relevances for supplied ids, given a dictionary of ratings
 # unrated items get a score of zero
 def get_relevances( ratings, ids ):
+    global relevance_bias
+
 	r = [ 0. ] * len( ids )
 	for i, x in enumerate( ids ):
 		try:
 			r[i] = ratings[x] - relevance_bias
 		except KeyError:
 			pass
-	
+
 	return r
-			
+
 
 # returns unsorted best possible relevances from the test set
 # if the list is shorter than ndcg_k, it gets padded with zeros
 def get_best_relevances( ratings, ids, ndcg_k ):
+    global relevance_bias
+
 	r = [ ratings[x] - relevance_bias for x in ids if x in ratings and ratings[x] > relevance_bias ]
-	
+
 	if len( r ) < ndcg_k:
 		return r + [ 0. ] * ( ndcg_k - len( r ))
 	else:
@@ -24,6 +28,8 @@ def get_best_relevances( ratings, ids, ndcg_k ):
 
 
 def get_worst_relevances( ratings, ids, ndcg_k ):
+    global relevance_bias
+
 	r = [ ratings[x] - relevance_bias for x in ids if x in ratings and ratings[x] < relevance_bias ]
 	if len( r ) < ndcg_k:
 		return r + [ 0. ] * ( ndcg_k - len( r ))
